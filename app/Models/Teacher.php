aa9e27aedbcb77c976f3a045cbe083bf93ca1367
applephagna <phagnaapple@gmail.com>
2020-01-08 17:57:55 +0700

    update
    update

---
+++ app/Models/Teacher.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Teacher
+ * @package App\Models
+ * @version January 8, 2020, 5:05 am UTC
+ *
+ * @property string first_name
+ * @property string last_name
+ * @property string gender
+ * @property string email
+ * @property string dob
+ * @property string phone
+ * @property string address
+ * @property string nationality
+ * @property string passport
+ * @property boolean status
+ * @property string dateregistered
+ * @property integer user_id
+ * @property string image
+ */
+class Teacher extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'teachers';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'dob',
+        'phone',
+        'address',
+        'nationality',
+        'passport',
+        'status',
+        'dateregistered',
+        'user_id',
+        'image'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'teacher_id' => 'integer',
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'gender' => 'string',
+        'email' => 'string',
+        'dob' => 'string',
+        'phone' => 'string',
+        'address' => 'string',
+        'nationality' => 'string',
+        'passport' => 'string',
+        'status' => 'boolean',
+        'dateregistered' => 'date',
+        'user_id' => 'integer',
+        'image' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'email' => 'required',
+        'dob' => 'required',
+        'status' => 'required',
+        'dateregistered' => 'required',
+        'user_id' => 'required'
+    ];
+
+    
+}
---
+++ app/Models/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class User
+ * @package App\Models
+ * @version January 8, 2020, 5:07 am UTC
+ *
+ * @property integer role_id
+ * @property string name
+ * @property string email
+ * @property string|\Carbon\Carbon email_verified_at
+ * @property string password
+ * @property string remember_token
+ */
+class User extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'users';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'role_id',
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'role_id' => 'integer',
+        'name' => 'string',
+        'email' => 'string',
+        'email_verified_at' => 'datetime',
+        'password' => 'string',
+        'remember_token' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'role_id' => 'required',
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+    ];
+
+    
+}
